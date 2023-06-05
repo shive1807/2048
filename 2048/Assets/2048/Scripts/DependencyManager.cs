46fe45c59a963c2b0ecb08081bafaac1e00f1687
fufu_games <shivambhati21@gmail.com>
2023-06-05 11:31:50 +0530

    added dependency manager to the prototype scene.

--- 2048/Assets/2048/Scripts/DependencyManager.cs
+++ 2048/Assets/2048/Scripts/DependencyManager.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class DependencyManager : MonoBehaviour
 {
     public static DependencyManager Instance { get; private set; }
+
     public GameController gameController;
     public GridController gridController;
     public InputManager inputManager;
+
     private void Awake()
     {
         Singleton();
     }
+
     private void Singleton()
     {
         if(Instance != null && Instance != this)
